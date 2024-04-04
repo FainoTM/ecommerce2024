03a6aa96ae083641edf818353f3901887aadd297
TheoFaino <theomfaino@gmail.com>
2024-04-03 22:15:22 -0400

    primeiro commit

--- catproduto/views.py
+++ catproduto/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, ListView, DetailView
 
 from catproduto.models import Categoria, Produto
 
@@ -13,12 +13,17 @@ class CategoriaListView(ListView):
     template_name = 'produtos/categorias.html'
     context_object_name = 'categorias'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produtos'] = Produto.objects.all()
+        return context
+
 
 class ProdutosListView(ListView):
     model = Produto
-    template_name = 'produtos/listarprodutos.html'
+    template_name = 'produtos/categorias.html'
     context_object_name = 'produtos'
-    # queryset = Produto.disponiveis.all()
+    queryset = Produto.disponiveis.all()
 
     def get_queryset(self): # removi o parâmetro daqui
         qs = super().get_queryset()
@@ -33,4 +38,19 @@ class ProdutosListView(ListView):
         slugcat = self.kwargs['slugcat']
         if slugcat:
             context['categoria'] = Categoria.objects.get(slug=slugcat)
+            context['categorias'] = Categoria.objects.all()
         return context
+
+
+class ProdutoDetailView(DetailView):
+    model = Produto
+    template_name = 'produtos/detalheProd.html'
+    context_object_name = 'produto'
+
+    def get_queryset(self):
+        return Produto.objects.disponiveis.all()
+
+    def get_object(self):
+        slug_prod = self.kwargs['slugProd']
+        return get_object_or_404(Produto, slug=slug_prod)
+
